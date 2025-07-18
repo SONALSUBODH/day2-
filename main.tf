0c0b21b4e8f625da91e67415990700c460564477
SONALSUBODH <ms.sonalkulkarni@gmail.com>
2025-07-18 18:33:03 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,24 @@
+resource "aws_security_group" "web_server_sg_tf" {
+  name        = "web-server-sg-tf"
+  description = "Allow HTTPS to web server"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    description = "HTTPS ingress"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
