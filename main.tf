2761aafb0bb62017ee8d7bce406e77394a558da6
SONALSUBODH <ms.sonalkulkarni@gmail.com>
2025-07-18 18:35:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,3 +22,13 @@ resource "aws_security_group" "web_server_sg_tf" {
 data "aws_vpc" "default" {
   default = true
 }
+
+
+resource "aws_instance" "web" {
+  ami           = "resolve:ssm:/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-default-x86_64"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
